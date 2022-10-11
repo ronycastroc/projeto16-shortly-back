b803d2c6d5e9e9b959b60965898d0d67f8a90804
ronycastroc <castroroony@gmail.com>
2022-10-11 19:26:05 -0300

    signin route

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -6,7 +6,7 @@ import joi from "joi";
 const signUpSchema = joi.object({
     name: joi.string().min(2).max(50).required(),
     email: joi.string().email().required(),
-    password: joi.string().required(),
+    password: joi.string().min(6).required(),
     confirmPassword: joi.string().required()
 });
 
@@ -36,7 +36,7 @@ const signUp = async (req, res) => {
 
         const passwordHash = bcrypt.hashSync(password, 10);
 
-        await connection.query('INSERT INTO users (name, email, password, confirmPassword) VALUES ($1, $2, $3, $4);', [name, email, passwordHash, passwordHash]);
+        await connection.query('INSERT INTO users (name, email, password) VALUES ($1, $2, $3);', [name, email, passwordHash]);
 
         res.sendStatus(201);
         
@@ -45,5 +45,42 @@ const signUp = async (req, res) => {
     }
 };
 
-export { signUp };
+const signInSchema = joi.object({
+    email: joi.string().email().required(),
+    password: joi.string().min(6).required()
+})
+
+const signIn = async (req, res) => {
+    const { email, password } = req.body;
+
+    const validation = signInSchema.validate(req.body, { abortEarly: false });
+
+    if(validation.error) {
+        const error = validation.error.details.map(value => value.message);
+        
+        return res.status(422).send(error);
+    }
+
+    try {
+        const user = (await connection.query('SELECT * FROM users WHERE email=$1;', [email])).rows;
+
+        if(user.length > 0 && bcrypt.compareSync(password, user[0].password)) {
+            const token = uuid();
+
+            await connection.query('DELETE FROM sessions WHERE "userId"=$1;', [user[0].id]);
+
+            await connection.query('INSERT INTO sessions ("userId", token) VALUES ($1, $2);', [user[0].id, token]);
+
+            res.status(200).send({ token });
+
+        } else {
+            res.sendStatus(401);
+        }
+
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+};
+
+export { signUp, signIn };
 
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { signUp } from "../controllers/authControllers.js";
+import { signUp, signIn } from "../controllers/authControllers.js";
 
 const router = express.Router();
 
 router.post("/signup", signUp);
+router.post("/signin", signIn);
 
 export default router;
